b63bfb387b88dbb9f30f99af04437015cc316aa9
brayan <230893@utxicotepec.edu.mx>
2024-12-07 21:10:24 -0600

    Agregar rutas para video

---
+++ toktik_230893/lib/infrastructure/repositories/video_posts_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'package:toktik_230893/domain/datasources/video_posts_datasource.dart';
+import 'package:toktik_230893/domain/entities/video_post.dart';
+import 'package:toktik_230893/domain/repositories/video_posts_repository.dart';
+
+
+
+class VideoPostsRepositoryImpl implements VideoPostRepository {
+
+  final VideoPostDatasource videosDatasource;
+
+  VideoPostsRepositoryImpl({
+    required this.videosDatasource
+  });
+
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videosDatasource.getTrendingVideosByPage(page);
+  }
+
+
+
+}
+
